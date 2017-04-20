b678d52bde04001cb2b2ce1d96b1a1edfda5f5d2
山田　空 <e1639@s.akashi.ac.jp>
2017-04-20 22:42:11 +0900

    if.c

--- if.c
+++ if.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 
-void main(){
-    int num;
-    printf("1～3の値を入力してください：");
-    //  キーボードから整数を入力
-    scanf("%d",&num);
-    //  入力した値が、正の数かどうかを調べる
-    if(num == 1){
-        printf("one\n");    //  numが1だった場合の処理
-    }else if(num == 2){
-        printf("two\n");    //  numが2だった場合の処理
-    }else if(num == 3){
-        printf("three\n");  //  numが3だった場合の処理
-    }else{
-        printf("不適切な値です。\n"); //  それ以外の値が入力された場合の処理
+int main(void){
+
+  int score = 50;
+
+  if (score >= 60) {
+     printf("OK!\n");
+  } else {
+      printf("NG!\n");
     }
-}
+   return(0);
+ }
