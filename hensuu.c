2509e606fa26eb4ef329f45f76cc18a705e206ac
山田　空 <e1639@s.akashi.ac.jp>
2017-04-20 22:21:09 +0900

    hensuu.c

--- hensuu.c
+++ hensuu.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
-/*
-    変数を用いた計算
-*/
 
-void main()
+int main(void)
 {
-    /* 使用する変数の定義 */
-    int a;  // 変数の宣言
-    int b = 3;      //  初期化と代入を同時に行う。
-    int add,sub;    //  複数の変数を同時に宣言
-    double avg;     //  int以外の変数を宣言
-    a = 6;  //  代入（最初に値を入れるので、”初期化”と言う。
-    add = a + b;            //  a,bの和を求める。
-    sub = a - b;            //  a,bの差を求める。
-    avg = (a + b) / 2.0;    //  a,bの平均値を求める。
-    printf("%d + %d = %d\n",a,b,add);
-    printf("%d - %d = %d\n",a,b,sub);
-    printf("%dと%dの平均値：%f\n",a,b,avg);
+  int x = 10;
+  float y = 5.2;
+  char c = 'A';
+
+  printf("x = %d, y = %f,c = %c\n", x, y, c);
+  return(0);
 }
